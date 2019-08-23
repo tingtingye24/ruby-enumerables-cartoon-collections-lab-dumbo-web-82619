72b4106566fe76e8567552241bebdad6776d7576
Ting Ting Ye <tingtingye24@gmail.com>
2019-08-23 03:53:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,13 @@ def summon_captain_planet(array)# code an argument here
     
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  array.each do |item|
+    if item.length > 4
+      TRUE
+    end
+    false 
 end
 
 def find_the_cheese# code an argument here
