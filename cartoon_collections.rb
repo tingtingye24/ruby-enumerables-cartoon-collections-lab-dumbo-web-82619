1df52fa31e2acd60c1c6709d2f472378c9e8e623
Ting Ting Ye <tingtingye24@gmail.com>
2019-08-23 03:48:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(array)# code an argument here
   # Your code here
   array.each_with_index do |index,dwarf|
-    puts "#{index}. #{dwarf}"
+    puts "#{index +1}. #{dwarf}"
   end
 end
 
