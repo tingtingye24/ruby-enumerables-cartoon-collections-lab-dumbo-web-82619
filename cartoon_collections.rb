7440a5e2744df1ac2f8421eccaa9b4d9aa27648b
Ting Ting Ye <tingtingye24@gmail.com>
2019-08-23 03:55:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,12 +13,7 @@ end
 
 def long_planeteer_calls(array)# code an argument here
   # Your code here
-  array.each do |item|
-    if item.length >= 4
-      return true
-    end
-    return false 
-  end
+  array.any? { |element| element.length > 4 }
 end
 
 def find_the_cheese# code an argument here
