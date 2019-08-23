76f6b5df93e9d4beedb8d0202fd595f4105fc282
Ting Ting Ye <tingtingye24@gmail.com>
2019-08-23 03:49:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,10 @@ def roll_call_dwarves(array)# code an argument here
   end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  array.each do |element|
+    
 end
 
 def long_planeteer_calls# code an argument here
