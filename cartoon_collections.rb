10f9db864ac3765c150941e7ed73db1a465fde37
Ting Ting Ye <tingtingye24@gmail.com>
2019-08-23 03:54:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,9 +15,9 @@ def long_planeteer_calls(array)# code an argument here
   # Your code here
   array.each do |item|
     if item.length > 4
-      TRUE
+      return TRUE
     end
-    false 
+    return false 
   end
 end
 
